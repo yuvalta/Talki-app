9c689c567c21dcd5ccaa29dbd21356698fd13f03
Yuval Tamir <tamir.yuval1@gmail.com>
2018-12-16 00:56:20 +0200

    2.12.18 - drawer menu, long press to change content, not saving changed content.

--- app/src/main/java/com/example/yuvaltamir/talki/MainActivity.java
+++ app/src/main/java/com/example/yuvaltamir/talki/MainActivity.java
@@ -1,48 +1,38 @@
 package com.example.yuvaltamir.talki;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewDebug;
-import android.view.Window;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
-import static android.widget.Toast.LENGTH_SHORT;
-
-// TODO check what about the return button on the device
-// TODO create different theme
-// TODO fix line 100
-// TODO why all of the new content is in CAPS?!
+//TODO fix the stop of animation while dialog is open - 2 different threads or AsyncTask
 
 public class MainActivity extends AppCompatActivity {
 
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
     Dialog dialogPrefix;
     Dialog changeContentDialog;
+    Dialog creditDialog;
     TextView actionText;
-    ImageView prefixOne;
-    ImageView prefixTwo;
-    ImageView prefixThree;
+    Button prefixOne;
+    Button prefixTwo;
+    Button prefixThree;
     DrawerLayout mDrawerLayout;
     RelativeLayout mainRelativeLayout;
     Button OKButton;
@@ -59,15 +49,26 @@ public class MainActivity extends AppCompatActivity {
     Button contentButton;
     TextInputLayout contentField;
     String newTextInput;
+    String chosenPrefix;
     Boolean errorInText = false;
     boolean isThemeChanged = false;
 
-    private String[] allAction = {"a","b","c","d","e","f","g","h","i"};
+    public static final int LENGTH_OF_ARRAY = 10;
+
+    public String[] allAction = new String[LENGTH_OF_ARRAY];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Animation background
+        relativeLayout = findViewById(R.id.main_relative_layout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
+
+        putDefaultValuesInArray(allAction); // Insert default values to all buttons
 
         final SharedPreferences actions = getSharedPreferences("preferences", MODE_PRIVATE);
 
@@ -84,24 +85,21 @@ public class MainActivity extends AppCompatActivity {
         // dialog
         dialogPrefix = new Dialog(this);
         changeContentDialog = new Dialog(this);
+        creditDialog = new Dialog(this);
         // drawer layout
         mDrawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                         menuItem.setChecked(true);
                         mDrawerLayout.closeDrawers();
 
                         if (menuItem.getItemId() == R.id.delete) {
                             actions.edit().clear().apply();
-
-                            for (int i = 0; i < allAction.length; i++) {
-                                allAction[i] = Integer.parseInt('a' + i); // update to default values - with ascii maybe
-                                Toast.makeText(getApplicationContext(), "All actions removed!", Toast.LENGTH_LONG).show();
-                            }
-
+                            putDefaultValuesInArray(allAction); // put default values in allAction
+                            Toast.makeText(getApplicationContext(), "All actions removed!", Toast.LENGTH_LONG).show();
                         }
 
                         if (menuItem.getItemId() == R.id.theme && !isThemeChanged) { // changing background color
@@ -111,23 +109,44 @@ public class MainActivity extends AppCompatActivity {
                             mainRelativeLayout.setBackgroundColor(getResources().getColor(R.color.white));
                             isThemeChanged = false;
                         }
+
+                        if (menuItem.getItemId() == R.id.credits) {
+                            creditDialog.setContentView(R.layout.credit_dialog);
+                            creditDialog.show();
+                        }
                         return true;
                     }
                 });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning())
+            animationDrawable.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning())
+            animationDrawable.stop();
+    }
+
     public void customPopupPrefix(View view) {
 
-        if(view.getId() == R.id.prefixOne) {
+        if (view.getId() == R.id.prefixOne) {
+            chosenPrefix = "Let's Go To ";
+
             dialogPrefix.setContentView(R.layout.popup_prefix_one);
 
             content1_1 = dialogPrefix.findViewById(R.id.content1_1);
             content1_2 = dialogPrefix.findViewById(R.id.content1_2);
             content1_3 = dialogPrefix.findViewById(R.id.content1_3);
 
-            content1_1.setText(allAction[0]);
-            content1_2.setText(allAction[1]);
-            content1_3.setText(allAction[2]);
+            content1_1.setText(allAction[1]);
+            content1_2.setText(allAction[2]);
+            content1_3.setText(allAction[3]);
 
             content1_1.setOnClickListener(onClickListener);
             content1_1.setOnLongClickListener(onLongClickListener);
@@ -137,16 +156,18 @@ public class MainActivity extends AppCompatActivity {
             content1_3.setOnLongClickListener(onLongClickListener);
         }
 
-        if(view.getId() == R.id.prefixTwo) {
+        if (view.getId() == R.id.prefixTwo) {
+            chosenPrefix = "I Want ";
+
             dialogPrefix.setContentView(R.layout.popup_prefix_two);
 
             content2_1 = dialogPrefix.findViewById(R.id.content2_1);
             content2_2 = dialogPrefix.findViewById(R.id.content2_2);
             content2_3 = dialogPrefix.findViewById(R.id.content2_3);
 
-            content2_1.setText(allAction[3]);
-            content2_2.setText(allAction[4]);
-            content2_3.setText(allAction[5]);
+            content2_1.setText(allAction[4]);
+            content2_2.setText(allAction[5]);
+            content2_3.setText(allAction[6]);
 
             content2_1.setOnClickListener(onClickListener);
             content2_1.setOnLongClickListener(onLongClickListener);
@@ -156,16 +177,18 @@ public class MainActivity extends AppCompatActivity {
             content2_3.setOnLongClickListener(onLongClickListener);
         }
 
-        if(view.getId() == R.id.prefixThree) {
+        if (view.getId() == R.id.prefixThree) {
+            chosenPrefix = "I Need ";
+
             dialogPrefix.setContentView(R.layout.popup_prefix_three);
 
             content3_1 = dialogPrefix.findViewById(R.id.content3_1);
             content3_2 = dialogPrefix.findViewById(R.id.content3_2);
             content3_3 = dialogPrefix.findViewById(R.id.content3_3);
 
-            content3_1.setText(allAction[6]);
-            content3_2.setText(allAction[7]);
-            content3_3.setText(allAction[8]);
+            content3_1.setText(allAction[7]);
+            content3_2.setText(allAction[8]);
+            content3_3.setText(allAction[9]);
 
             content3_1.setOnClickListener(onClickListener);
             content3_1.setOnLongClickListener(onLongClickListener);
@@ -175,6 +198,19 @@ public class MainActivity extends AppCompatActivity {
             content3_3.setOnLongClickListener(onLongClickListener);
         }
         dialogPrefix.show();
+    }
+
+    public void putDefaultValuesInArray(String[] array) {
+        array[0] = " ";
+        array[1] = "The Bar";
+        array[2] = "The Stage";
+        array[3] = "The Bathroom";
+        array[4] = "To Drink";
+        array[5] = "To Eat";
+        array[6] = "To Dance";
+        array[7] = "To Go Home";
+        array[8] = "To Pee";
+        array[9] = "To Puke";
     }
 
     private View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -188,12 +224,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onLongClick(View view) {
 
-
-
             changeContentDialog.setContentView(R.layout.change_content_text);
 
             final InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
 
             contentField = changeContentDialog.findViewById(R.id.contentField);
             OKButton = changeContentDialog.findViewById(R.id.OKButton);
@@ -205,7 +239,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View view) {
 
                     pressedButton = (Button) view;
-                    newTextInput = contentField.getEditText().getText().toString().trim(); // get the text from the text box
+                    newTextInput = Objects.requireNonNull(contentField.getEditText()).getText().toString().trim(); // get the text from the text box
 
                     if (newTextInput.isEmpty()) { // checks if the text is legal
                         errorInText = true;
@@ -223,13 +257,11 @@ public class MainActivity extends AppCompatActivity {
 
                         updateNewContentToSharedPreferences(allAction);
 
-                        imm.toggleSoftInput(InputMethodManager.RESULT_HIDDEN,0); // close keyboard
+                        imm.toggleSoftInput(InputMethodManager.RESULT_HIDDEN, 0); // close keyboard
 
                         changeContentDialog.dismiss();
-                    }
-                    else{
+                    } else {
                         errorInText = false;
-                        return;
                     }
                 }
             });
@@ -242,7 +274,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateNewContentToSharedPreferences(String[] arrayOfActions) {
         StringBuilder stringBuilder = new StringBuilder();
-        for(String s : arrayOfActions) {
+        for (String s : arrayOfActions) {
             stringBuilder.append(s);
             stringBuilder.append(",");
         }
@@ -250,13 +282,15 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences actions = getSharedPreferences("preferences", MODE_PRIVATE);
         SharedPreferences.Editor editor = actions.edit();
         editor.putString("Action", stringBuilder.toString());
-        editor.commit();
+        editor.apply();
     }
 
     public void loadContentToSharedPreferences(String[] arrayOfActions) {
 
         SharedPreferences actions = getSharedPreferences("preferences", MODE_PRIVATE);
         String stringOfActions = actions.getString("Action", "");
+        if (stringOfActions == null) // checks if the string is null
+            throw new AssertionError();
         String[] splitActions = stringOfActions.split(",");
 
         for (int i = 0; i < splitActions.length; i++) {
@@ -274,7 +308,7 @@ public class MainActivity extends AppCompatActivity {
         prefixTwo.setVisibility(View.INVISIBLE);
         prefixThree.setVisibility(View.INVISIBLE);
 
-        actionText.setText(pressedButton.getText().toString());
+        actionText.setText(chosenPrefix + pressedButton.getText().toString());
         actionText.setVisibility(View.VISIBLE);
     }
 
@@ -284,6 +318,7 @@ public class MainActivity extends AppCompatActivity {
         prefixThree.setVisibility(View.VISIBLE);
         actionText.setVisibility(View.INVISIBLE);
         actionText.setText(" ");
+        chosenPrefix = " ";
     }
 
 }
